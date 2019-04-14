f8b31ebcc071c67d712d668ade8d0cf4278e7386
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-14 15:55:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,3 +10,8 @@ def using_pop(array)
   popped_element = array.pop
   return popped_element
 end 
+
+def pop_with_args(array)
+  last_two_popped_elements = []
+  popped_element1 = array.popped
+  return pop
